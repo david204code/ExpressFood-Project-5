5cb23355a9820897d0076da643d27a3a4d7ff8c9
David Wu <david204code@gmail.com>
2018-12-07 22:10:58 +0000

    On the Addresses Schema, I have now added state, country and postcode attribute

--- ExpressFoodSchema.sql
+++ ExpressFoodSchema.sql
@@ -11,5 +11,8 @@ CREATE TABLE Clients (
 CREATE TABLE Addresses (
   addressId INT PRIMARY KEY AUTO_INCREMENT,
   address VARCHAR(40),
-  city VARCHAR(15)
+  city VARCHAR(15),
+  state VARCHAR(10),
+  country VARCHAR(15),
+  postCode VARCHAR(8)
 );
