0459020f1ca9a0486b38aa2f5bf156cf1ecfa3cd
David Wu <david204code@gmail.com>
2018-12-07 21:22:27 +0000

    Added the email attribute

--- ExpressFoodSchema.sql
+++ ExpressFoodSchema.sql
@@ -2,6 +2,7 @@
 
 CREATE TABLE Clients (
   clientId INT PRIMARY KEY AUTO_INCREMENT,
-  firstName VARCHAR(20) NOT NULL
-  lastName VARCHAR(20) NOT NULL 
+  firstName VARCHAR(20) NOT NULL,
+  lastName VARCHAR(20) NOT NULL,
+  email VARCHAR(50)
 );
