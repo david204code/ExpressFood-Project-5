41791a703233be057bafc11536287f614153d582
David Wu <david204code@gmail.com>
2018-12-13 15:09:30 +0000

    All schemas reorder so it can be run in a single click

--- ExpressFoodSchema.sql
+++ ExpressFoodSchema.sql
@@ -1,12 +1,21 @@
 
+DROP TABLE IF EXISTS Addresses;
+DROP TABLE IF EXISTS OrderItems;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Clients;
+DROP TABLE IF EXISTS Delivers;
+DROP TABLE IF EXISTS Menu;
+
 
 CREATE TABLE Clients (
   clientId INT PRIMARY KEY AUTO_INCREMENT,
   firstName VARCHAR(20) NOT NULL,
   lastName VARCHAR(20) NOT NULL,
   email VARCHAR(50),
-  phone VARCHAR(20)
+  phone VARCHAR(20) NOT NULL
 );
+
+-- DESCRIBE Clients;
 
 CREATE TABLE Addresses (
   addressId INT PRIMARY KEY AUTO_INCREMENT,
@@ -19,49 +28,63 @@ CREATE TABLE Addresses (
   FOREIGN KEY (clientId) REFERENCES Clients(clientId)
 );
 
-CREATE TABLE Orders (
-  orderId INT PRIMARY KEY AUTO_INCREMENT,
-  clientId INT,
-  driverId INT,
-  totalPrice FLOAT(4,2),
-  status ENUM('Preparing', 'Cooking', 'Cancel', 'Delievery'),
-  date DATETIME,
-  paymentMethod ENUM('Cash', 'Paypal', 'Credit Card'),
-  address VARCHAR(40),
-  city VARCHAR(15),
-  state VARCHAR(10),
-  country VARCHAR(15),
-  postCode VARCHAR(8),
-  FOREIGN KEY (clientId) REFERENCES Clients(clientId)
-);
+-- DESCRIBE Addresses;
 
-ALTER TABLE Orders
-ADD FOREIGN KEY(driverId)
-REFERENCES Delievers(driverId)
-ON DELETE SET NULL;
-
-CREATE TABLE Delievers (
-  driverId INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Delivers (
+  deliverId INT PRIMARY KEY AUTO_INCREMENT,
   firstName VARCHAR(20),
   lastName VARCHAR(20),
   phone VARCHAR(20)
 );
 
-CREATE TABLE OrderItems (
-  orderItemId INT PRIMARY KEY AUTO_INCREMENT,
-  orderId INT,
-  itemId INT, 
-  quantity INT,
-  FOREIGN KEY (orderId) REFERENCES Orders(orderId) 
+-- DESCRIBE Delivers;
+
+CREATE TABLE Orders (
+  orderId INT PRIMARY KEY AUTO_INCREMENT,
+  clientId INT,
+  deliverId INT,
+  totalPrice FLOAT(4,2),
+  status ENUM('pending', 'processing', 'delievered', 'cancelled'),
+  date DATETIME,
+  paymentMethod ENUM('cash', 'paypal', 'credit_card'),
+  address VARCHAR(40),
+  city VARCHAR(15),
+  state VARCHAR(10),
+  country VARCHAR(15),
+  postCode VARCHAR(8),
+  FOREIGN KEY (clientId) REFERENCES Clients(clientId),
+  FOREIGN KEY (deliverId) REFERENCES Delivers(deliverId)
 );
 
-ALTER TABLE OrderItems
-ADD FOREIGN KEY (itemId) REFERENCES Menu(itemId)
+-- DESCRIBE Orders;
 
 CREATE TABLE Menu (
-  itemId INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(10),
-  description VARCHAR(100),
-  price FLOAT(4,2),
-  active TINYINT(1)
+    itemId INT PRIMARY KEY AUTO_INCREMENT,
+    type ENUM('main', 'dessert'),
+    name VARCHAR(10),
+    description VARCHAR(100),
+    price FLOAT(4,2),
+    active TINYINT(1)
 );
+
+-- DESCRIBE Menu;
+
+CREATE TABLE OrderItems (
+    orderItemId INT PRIMARY KEY AUTO_INCREMENT,
+    orderId INT,
+    itemId INT,
+    quantity INT,
+    FOREIGN KEY (orderId) REFERENCES Orders(orderId),
+    FOREIGN KEY (itemId) REFERENCES Menu(itemId)
+);
+
+  -- DESCRIBE OrderItems;
+
+-- INSERT CLIENTS
+INSERT INTO Clients
+INSERT INTO Clients
+INSERT INTO Clients
+INSERT INTO Clients
+
+-- INSERT ADDRESSES
+INSERT INTO Addresses;
